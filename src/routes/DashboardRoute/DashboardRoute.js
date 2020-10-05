87925a7dc83b57e53205ef1130df5b1db0f49802
Jackie Abert <jax.abert@gmail.com>
2020-10-05 10:59:00 -0400

    commit

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class DashboardRoute extends Component {
+  //need to pull info Language, word, correct, and incorrect from server
+  
   render() {
     return (
       <section>
-        implement and style me
+        <h2>Language will go here</h2>
+        <ul>Words to Practice
+          <li>Word 1 : correct / incorrect</li>
+          <li>Word 2 : correct / incorrect</li>
+          <li>Word 3 : correct / incorrect</li>
+          <li>Word 4 : correct / incorrect</li>
+          <li>Word 5 : correct / incorrect</li>
+          <li>Word 6 : correct / incorrect</li>
+        </ul>
+        <Link to='/learn'><button>Start Practicing</button></Link>
       </section>
     );
   }
