37ab4e308753b422a01ea005dd4950804f8f10e8
Jackie Abert <jax.abert@gmail.com>
2020-10-06 10:34:51 -0400

    api

---
+++ src/services/language-api-service.js
@@ -0,0 +1,18 @@
+import config from "../config";
+import TokenService from "./token-service";
+
+const languageService = {
+    getLanguage() {
+        return fetch(`${config.API_ENDPOINT}/api/language`, {
+            method: "GET",
+            headers: {
+                authorization: `bearer ${TokenService.getAuthToken()}`,
+                "Content-Type":"application/json",
+            },
+        }).then((res) =>
+        !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+      );
+    }
+}
+
+export default languageService
