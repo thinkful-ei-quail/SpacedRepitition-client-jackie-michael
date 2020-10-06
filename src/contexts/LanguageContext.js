836d37228e96f3817a49661ffbe887c32cb7cdda
Jackie Abert <jax.abert@gmail.com>
2020-10-06 14:57:23 -0400

    context

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,54 +1,45 @@
-import languageService from '../services/language-api-service'
-import React, { Component } from 'react'
+import languageService from "../services/language-api-service";
+import React, { Component } from "react";
 
 const LanguageContext = React.createContext({
-    language:{},
+  language: {},
+  words: {},
+});
+
+export default LanguageContext;
+
+export class LanguageProvider extends Component {
+  state = {
+    language: {},
     words:[]
-  })
- 
-  export default LanguageContext
-
-  export class LanguageProvider extends Component {
-    state = {
-        language:{}
-    }
-    // importLanguage = () => {
-    //     const language = languageService.getLanguage()
-    //     console.log(language)
-    //     this.setLanguage({
-    //         language:language
-    //     })
-    // }
-    setLanguage = language => {
-        this.setState({ language })
-      }
-      setWords = words => {
-          this.setState({ words })
-      }
-componentDidMount(){
-    languageService.getLanguage()
-    .then(language => {
-        this.setState({
-        language,
-        words:language.words
-    })
-    console.log(language)
-})
-}
-
-    render() {
-        const value = {
-          language: this.state.language,
-          words: this.state.words,
-        //   error: this.error,
-        //   setError: this.setError,
-        //   clearError: this.clearError
-        }
-        console.log(value)
-        return (
-          <LanguageContext.Provider value={value}>
-            {this.props.children}
-          </LanguageContext.Provider>
-        )
-      }
+  };
+  setLanguage = (language) => {
+    this.setState({ language });
+  };
+  setWords = (words) => {
+    this.setState({ words });
+  };
+  componentDidMount() {
+    languageService.getLanguage().then((data) => {
+      this.setState({
+        language: data.language,
+        words: data.words,
+      });
+    });
   }
+
+  render() {
+    const value = {
+      language: this.state.language,
+      words: this.state.words,
+      //   error: this.error,
+      //   setError: this.setError,
+      //   clearError: this.clearError
+    };
+    return (
+      <LanguageContext.Provider value={value}>
+        {this.props.children}
+      </LanguageContext.Provider>
+    );
+  }
+}
--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,46 +1,54 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import LanguageContext from '../../contexts/LanguageContext'
-import LanguageService from '../../services/language-api-service'
-import './dashboardroute.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import LanguageContext from "../../contexts/LanguageContext";
+import LanguageService from "../../services/language-api-service";
+import "./dashboardroute.css";
 
 class DashboardRoute extends Component {
-  //need to pull info Language, word, correct, and incorrect from server 
-  static defaultProps = {
-    language:{}
-  }
-  static contextType = LanguageContext
+  static contextType = LanguageContext;
 
-renderWordsToPractice(){
-  const { language, words } = this.context
-  console.log('Language', language)
-  console.log('Words', words)
-  if (!language || !words) {
-    return null;
-  }
-  return (
-        <ul>
+  renderWordsToPractice() {
+    const { words } = this.context;
+    console.log("Words", words);
+    if (!words) {
+      return null;
+    }
+    return (
+      <ul>
         {words.map((word) => (
-          <li key={word.id} >
-          {word.translation}: Correct: {word.correct_count} / Incorrect: {word.incorrect_count}
+          <li key={word.id}>
+            {word.original}: Correct: {word.correct_count} / Incorrect:{" "}
+            {word.incorrect_count}
           </li>
         ))}
-        </ul>
-  )
-}
+      </ul>
+    );
+  }
+  renderLanguageName() {
+    const language = this.context.language;
+    console.log("language", language);
+    if (!language) {
+      return null;
+    }
+    return <h2>{language.name}</h2>;
+  }
 
   render() {
     return (
       <section className="dashboard">
-        <h2>Test language 1</h2>
+        {this.renderLanguageName()}
         <fieldset className="dashboardFieldset">
           <legend>Words to Practice</legend>
-        {this.renderWordsToPractice()}
-        <section className="startPracticeButton"><Link to='/learn'><button>Start Practicing</button></Link></section>
+          {this.renderWordsToPractice()}
+          <section className="startPracticeButton">
+            <Link to="/learn">
+              <button>Start Practicing</button>
+            </Link>
+          </section>
         </fieldset>
       </section>
     );
   }
 }
 
-export default DashboardRoute
+export default DashboardRoute;
